045227e7fb1c91514113751565596df695371018
sundiata keita <sundiatakeita@Suaibus-MacBook-Air.local>
2022-05-27 23:26:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+
+#Number =6
+#Number2 =3
+#result =int( Number * Number2)
+#print(result)
+
+
+#c = 125
+#F = (c * 9/5) + 32
+#print(F)
+
+#Question 1
+# x = 10
+# x = 20
+# x = 15
+# print(x)
+
+# Question 2
+# x = 100
+# y = 50
+# z = x/y
+# print(z)
+
+# Question 3
+# length = 6
+# b = 5
+# area = length * b
+# print(area)
+
+# Question 4
+# x = 25
+# y = 20
+# z = 3
+# f = 2
+# print((x - y) * z/f)
+
+# Question 5
+# number = 20
+# print(20/3)
+
+# Question 6
+import math
+# x = 4 ** 2
+# print(x)
+# print(math.sqrt(x))
+
+# Question 7
+# x = 5
+# sides = 6 * (x * x)
+# volume = x * x * x
+# print(sides)
+# print(volume)
+
+# Queston 8
+# x = 50
+# print(x * 3)
+
+# Question 10
+# x = 58
+# y = 2
+#
+# if x > y:
+#     answer = (x - y) * (x + y)
+#     print(answer)
+# else:
+#     answer = (y - x) * (x + y)
+#     print(answer)
+
+# Question 12
+# a = 2
+# b = 3
+# h = 5
+# Area = 1/2 * (b * h)
+# print(f"The area is {Area}")
+#
+# parameter = (a * 2) + b
+# print(f"The parameter is {parameter}")
+
+# Question 13
+# r = 7
+# Pi = 22/7
+# Area = Pi * r**2
+# print(f"The area is {Area}")
+# perimeter = 2*Pi*r
+# print(f"The perimeter is {perimeter}")
+
+
+# this are variables jhbdsfhbdfhbdsf
+
+# jibril_baldeh = 1
+# jibrilBaldeh = 3
+# _jibrilBaldeh = 7
+# value = 4.6
+# list = [1, 3 , 5,  "baldeh"]
+# print(list)
+# print(type(list))
+# print(type(value))
+#
+# numbers = "123"
+# name = "Jibril balldeh"
+# print(name)
+# print(type(name))
+# print(type(numbers))
+# print(type(jibril_baldeh))
+#
+#
+# print(jibril_baldeh)
+# print(jibrilBaldeh)
+# print(_jibrilBaldeh)
+
+
+
+
+# x = 1
+# y = 4
+# z = x + y
+# print(z)
+
+
+# x = 8
+# z = 3
+# h = round(x / z)
+# print(h)
+
+#
+# l = int(input("Enter your length:  "))
+#
+# b = int(input("Enter the breath: "))
+#
+# area = l * b
+# perimeter = 2 * (l + b)
+# print(f'your area is {area} \n Your perimeter is {perimeter}' )
+
+# age =  int(input("Enter your age: "))
+# age_remaining = 90 - age
+# remaining_days = age_remaining * 365
+# remaining_weeks = age_remaining * 52
+#
+# print(f"You have {age_remaining} years remaining from your age\n "
+#       f"You have {remaining_weeks} weeks remaining in your age\n "
+#       f"You have {remaining_days} days remaining in age")
+
+
+
+# y = "sundiata"
+# z = "keita"
+# f = y + " " + z
+# l = f.split('@')
+# print(l)
+
+y = 'keitajat30o@gmail.com'
+z = y.split('@')
+print(type(z))
+print(z[1])
+print(z)
+
+
